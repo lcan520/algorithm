f6717fd6417cbb9d44f8d44a0d4f07817f8ecd71
lcan520 <zhilong.li@cisdi.com.cn>
2022-07-11 14:51:16 +0800

    Add：新的算法

---
+++ src/main/java/com/algorithm/leetcode/editor/cn/P16ThreeSumClosest.java
@@ -0,0 +1,104 @@
+//给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。 
+//
+// 返回这三个数的和。 
+//
+// 假定每组输入只存在恰好一个解。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-1,2,1,-4], target = 1
+//输出：2
+//解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,0,0], target = 1
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 3 <= nums.length <= 1000 
+// -1000 <= nums[i] <= 1000 
+// -10⁴ <= target <= 10⁴ 
+// 
+//
+// Related Topics 数组 双指针 排序 👍 1187 👎 0
+
+
+package com.algorithm.leetcode.editor.cn;
+import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.date.DateUtil;
+
+import java.util.Arrays;
+
+public class P16ThreeSumClosest {
+    public static void main(String[] args) {
+        Solution solution = new P16ThreeSumClosest().new Solution();
+        TimeInterval timer = DateUtil.timer();
+        // TODO 代码调用solution.<method>
+        solution.threeSumClosest(new int[]{-763,567,885,-628,-530,-468,549,-341,275,-282,22,-448,-804,-369,636,-677,-907,898,8,-643,213,245,756,-328,-987,-433,639,-18,-104,-439,715,-145,-703,299,-84,767,818,-300,-374,232,962,-289,638,144,-508,870,480,-70,-576,661,-808}, 1);
+        long interval = timer.interval();
+        System.out.println("interval = " + interval + "ms");
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int best = 10000000;
+
+        // 枚举 a
+        for (int i = 0; i < n; ++i) {
+            // 保证和上一次枚举的元素不相等
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            // 使用双指针枚举 b 和 c
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                // 如果和为 target 直接返回答案
+                if (sum == target) {
+                    return target;
+                }
+                // 根据差值的绝对值来更新答案
+                if (Math.abs(sum - target) < Math.abs(best - target)) {
+                    best = sum;
+                }
+                if (sum > target) {
+                    // 如果和大于 target，移动 c 对应的指针
+                    int k0 = k - 1;
+                    // 移动到下一个不相等的元素
+                    while (j < k0 && nums[k0] == nums[k]) {
+                        --k0;
+                    }
+                    k = k0;
+                } else {
+                    // 如果和小于 target，移动 b 对应的指针
+                    int j0 = j + 1;
+                    // 移动到下一个不相等的元素
+                    while (j0 < k && nums[j0] == nums[j]) {
+                        ++j0;
+                    }
+                    j = j0;
+                }
+            }
+        }
+        return best;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+    
+}   
+
+ 
