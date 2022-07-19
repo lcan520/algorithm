bca5b830f8e391f9bda0808829eacbecab3f6bfa
lcan520 <zhilong.li@cisdi.com.cn>
2022-07-19 18:13:45 +0800

    Add：P27、P30、P86

--- src/main/java/com/algorithm/leetcode/editor/cn/P30SubstringWithConcatenationOfAllWords.java
+++ src/main/java/com/algorithm/leetcode/editor/cn/P30SubstringWithConcatenationOfAllWords.java
@@ -47,23 +47,60 @@ package com.algorithm.leetcode.editor.cn;
 import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.date.DateUtil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class P30SubstringWithConcatenationOfAllWords {
     public static void main(String[] args) {
         Solution solution = new P30SubstringWithConcatenationOfAllWords().new Solution();
         TimeInterval timer = DateUtil.timer();
         // TODO 代码调用solution.<method>
+        solution.findSubstring("barfoothefoobarman", new String[]{"foo", "bar"});
         long interval = timer.interval();
         System.out.println("interval = " + interval + "ms");
     }
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+        int m = words.length, n = words[0].length(), ls = s.length();
+        for (int i = 0; i < n; i++) {
+            if (i + m * n > ls) {
+                break;
+            }
+            Map<String, Integer> differ = new HashMap<>();
+            for (int j = 0; j < m; j++) {
+                String word = s.substring(i + j * n, i + (j + 1) * n);
+                differ.put(word, differ.getOrDefault(word, 0) + 1);
+            }
+            for (String word : words) {
+                differ.put(word, differ.getOrDefault(word, 0) - 1);
+                if (differ.get(word) == 0) {
+                    differ.remove(word);
+                }
+            }
+            for (int start = i; start < ls - m * n + 1; start += n) {
+                if (start != i) {
+                    String word = s.substring(start + (m - 1) * n, start + m * n);
+                    differ.put(word, differ.getOrDefault(word, 0) + 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                    word = s.substring(start - n, start);
+                    differ.put(word, differ.getOrDefault(word, 0) - 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                }
+                if (differ.isEmpty()) {
+                    res.add(start);
+                }
+            }
+        }
+        return res;
 
-
-
-        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/java/com/algorithm/leetcode/editor/cn/P86PartitionList.java
@@ -0,0 +1,88 @@
+//给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。 
+//
+// 你应当 保留 两个分区中每个节点的初始相对位置。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,4,3,2,5,2], x = 3
+//输出：[1,2,2,4,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [2,1], x = 2
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 200] 内 
+// -100 <= Node.val <= 100 
+// -200 <= x <= 200 
+// 
+//
+// Related Topics 链表 双指针 👍 590 👎 0
+
+
+package com.algorithm.leetcode.editor.cn;
+import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.date.DateUtil;
+
+public class P86PartitionList {
+    public static void main(String[] args) {
+        Solution solution = new P86PartitionList().new Solution();
+        TimeInterval timer = DateUtil.timer();
+        // TODO 代码调用solution.<method>
+        long interval = timer.interval();
+        System.out.println("interval = " + interval + "ms");
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+}   
+
+ 
